ce1785690eb624055a0aa2154b113c0f59c08ed7
JackTSully <112397886+JackTSully@users.noreply.github.com>
2023-11-12 16:23:17 +0700

    Add files via upload

---
+++ src/Dependencies.py
@@ -0,0 +1,20 @@
+import pygame
+from src.Util import SpriteManager, Animation
+import src.Util as Util
+from src.StateMachine import StateMachine
+
+sprite_collection = SpriteManager().spriteCollection
+
+gFonts = {
+    'small': pygame.font.Font('fonts/font.ttf', 24),
+    'medium': pygame.font.Font('fonts/font.ttf', 48),
+    'large': pygame.font.Font('fonts/font.ttf', 96),
+    'zelda_small': pygame.font.Font('fonts/zelda.otf', 96),
+    'zelda': pygame.font.Font('fonts/zelda.otf', 128),
+    'gothic_medium': pygame.font.Font('fonts/GothicPixels.ttf', 48),
+    'gothic_large': pygame.font.Font('fonts/GothicPixels.ttf', 96),
+
+}
+
+from src.states.game.StartState import StartState
+from src.StateMachine import StateMachine
