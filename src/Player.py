58ec1881b5aeca7f8a4a6cc80757652bb0a55e3b
JackTSully <112397886+JackTSully@users.noreply.github.com>
2023-11-12 23:08:18 +0700

    Add files via upload

---
+++ src/Player.py
@@ -0,0 +1,31 @@
+import pygame
+from src.Dependencies import *
+
+class Player():
+    def __init__(self,max_health):
+        self.x = None
+        self.y = None
+
+        self.max_health = max_health
+        self.cur_health = self.max_health
+        #ablity_deck = Deck("ability")
+        self.sprite = pygame.image.load("graphics/char/paladin.png")
+
+    def attack(self, monster, DN):
+        pass
+
+    def heal_self(self, amount):
+        self.cur_health += amount
+    
+    def setXY(self, x: int = None, y: int = None):
+        if x != None:
+            self.x = x
+        if y != None:
+            self.y = y
+
+    def update(self, dt, events):
+        if self.cur_health > self.max_health:
+            self.cur_health = self.max_health
+    
+    def render(self, screen):
+        screen.blit(self.sprite,(self.x,self.y))
