cadf58749f0db793a45b2791e5504c09c0eb1fc6
JackTSully <112397886+JackTSully@users.noreply.github.com>
2023-11-13 02:30:29 +0700

    Add files via upload

--- src/states/game/RestState.py
+++ src/states/game/RestState.py
@@ -12,11 +12,13 @@ class RestState(BaseState):
         self.bg_image = pygame.transform.scale(
             self.bg_image, (WIDTH + 5, HEIGHT + 5))
         self.floor = 0
+        self.time_interval = 3
+        self.timer = 0
 
     def Enter(self,params):
         if self.floor == 0:
             self.player = Player(30)
-            self.player.setXY(50,50)
+            self.player.setXY(WIDTH/3 - 50 ,HEIGHT/3)
             #floors = {
             #   "The Mines": Floor(1, floor1_monster_deck)
             # }
@@ -35,8 +37,22 @@ class RestState(BaseState):
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+        
+        self.timer = self.timer + dt
+
 
     def render(self, screen):
-        screen.blit(self.bg_image, (0, 0))
+        screen.blit(self.bg_image, (0, 0)) 
+
         
+        if self.timer < self.time_interval:
+            t_press_enter = gFonts['minecraft'].render("Rest Area, Floor 1", False, (255, 255, 255))
+            rect = t_press_enter.get_rect(center=(WIDTH / 2, HEIGHT / 2 -192))
+            screen.blit(t_press_enter, rect)
+
+        if self.timer > self.time_interval and self.timer < self.time_interval*2:
+            t_press_enter = gFonts['minecraft'].render("Discard 2 Cards", False, (255, 255, 255))
+            rect = t_press_enter.get_rect(center=(WIDTH / 2, HEIGHT / 2 -192))
+            screen.blit(t_press_enter, rect)
+
         self.player.render(screen)
