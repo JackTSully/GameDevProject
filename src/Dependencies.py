38a4bc716b324c74fe7b1511da070a2f34227d58
JackTSully <6322773308@g.siit.tu.ac.th>
2023-11-14 15:16:17 +0700

    dependencies

--- src/Dependencies.py
+++ src/Dependencies.py
@@ -35,6 +35,14 @@ gFonts = {
     'zelda': pygame.font.Font('fonts/zelda.otf', 128),
     'gothic_medium': pygame.font.Font('fonts/GothicPixels.ttf', 48),
     'gothic_large': pygame.font.Font('fonts/GothicPixels.ttf', 96),
+    'minecraft': pygame.font.Font('fonts/Minecraft.ttf', 96),
+    'dungeon': pygame.font.Font('fonts/DUNGRG__.TTF', 200),
 
 }
+
+from src.states.game.StartState import StartState
+from src.states.game.RestState import RestState
+from src.states.game.MapState import MapState
+from src.states.game.CombatState import CombatState
+from src.StateMachine import StateMachine
 
