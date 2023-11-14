c4ef79a70729625bb5cdf3cd9b162ed025e15c8d
rachanoniny <113294401+rachanoniny@users.noreply.github.com>
2023-11-14 18:54:00 +0700

    -

--- src/Enemies.py
+++ src/Enemies.py
@@ -1,12 +1,18 @@
 import pygame
 from src.Dependencies import *
 
-class Player():
-    def __init__(self,max_health):
+class Enemies():
+    def __init__(self, card_id, name, description, max_health, attack_dice, attack_bonus, e_ability_id):
+        self.card_id = card_id
+        self.name = name
+        self.description = description
+        self.max_health = max_health
+        self.attack_dice = attack_dice
+        self.attack_bonus = attack_bonus
+        self.e_ability_id = e_ability_id
+
         self.x = None
         self.y = None
-
-        self.max_health = max_health
         self.cur_health = self.max_health
         #ablity_deck = Deck("ability")
 
